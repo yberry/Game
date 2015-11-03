29813ae9202517e62bb94d80a4b700968566e092
yberry <y_berry@hotmail.fr>
2015-11-03 15:42:27 +0100

    Rajout de x et y

--- Trou.cpp
+++ Trou.cpp
@@ -1,23 +1,27 @@
 #include "stdafx.h"
 #include "Trou.h"
 
+int Trou::largeur = 5;
+int Trou::hauteur = 4;
 
-Trou::Trou() :
-taupe(false)
+Trou::Trou(int x, int y) :
+m_x(x),
+m_y(y),
+m_taupe(false)
 {
 }
 
-void Trou::draw(CHAR_INFO* buffer, COORD bufferSize, int x, int y, char lettre) {
-	(buffer + ((x + 1)*bufferSize.X + y))->Char.AsciiChar = lettre;
-	(buffer + ((x + 2)*bufferSize.X + y))->Char.AsciiChar = lettre;
-	(buffer + ((x + 1)*bufferSize.X + y + 4))->Char.AsciiChar = lettre;
-	(buffer + ((x + 2)*bufferSize.X + y + 4))->Char.AsciiChar = lettre;
-	(buffer + (x*bufferSize.X + y + 1))->Char.AsciiChar = lettre;
-	(buffer + (x*bufferSize.X + y + 2))->Char.AsciiChar = lettre;
-	(buffer + (x*bufferSize.X + y + 3))->Char.AsciiChar = lettre;
-	(buffer + ((x + 3)*bufferSize.X + y + 1))->Char.AsciiChar = lettre;
-	(buffer + ((x + 3)*bufferSize.X + y + 2))->Char.AsciiChar = lettre;
-	(buffer + ((x + 3)*bufferSize.X + y + 3))->Char.AsciiChar = lettre;
+void Trou::draw(CHAR_INFO* buffer, COORD bufferSize, char lettre) const {
+	(buffer + ((m_x + 1)*bufferSize.X + m_y))->Char.AsciiChar = lettre;
+	(buffer + ((m_x + 2)*bufferSize.X + m_y))->Char.AsciiChar = lettre;
+	(buffer + ((m_x + 1)*bufferSize.X + m_y + 4))->Char.AsciiChar = lettre;
+	(buffer + ((m_x + 2)*bufferSize.X + m_y + 4))->Char.AsciiChar = lettre;
+	(buffer + (m_x*bufferSize.X + m_y + 1))->Char.AsciiChar = lettre;
+	(buffer + (m_x*bufferSize.X + m_y + 2))->Char.AsciiChar = lettre;
+	(buffer + (m_x*bufferSize.X + m_y + 3))->Char.AsciiChar = lettre;
+	(buffer + ((m_x + 3)*bufferSize.X + m_y + 1))->Char.AsciiChar = lettre;
+	(buffer + ((m_x + 3)*bufferSize.X + m_y + 2))->Char.AsciiChar = lettre;
+	(buffer + ((m_x + 3)*bufferSize.X + m_y + 3))->Char.AsciiChar = lettre;
 }
 
 
