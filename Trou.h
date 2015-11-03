c8f144992ecee72ab8cce76fcdfbe6859a34304b
yberry <y_berry@hotmail.fr>
2015-11-03 15:42:45 +0100

    Update Trou.h

--- Trou.h
+++ Trou.h
@@ -5,13 +5,19 @@
 class Trou
 {
 private:
-	bool taupe;
+	bool m_taupe;
+	int m_x;
+	int m_y;
+	static int largeur;
+	static int hauteur;
 
 public:
-	Trou();
-	void draw(CHAR_INFO* buffer, COORD bufferSize, int x, int y, char lettre);
-	bool getTaupe() const { return this->taupe; }
-	void setTaupe(bool the_taupe) { this->taupe = the_taupe; }
+	Trou(int x, int y);
+	void draw(CHAR_INFO* buffer, COORD bufferSize, char lettre) const;
+	bool getTaupe() const { return this->m_taupe; }
+	void setTaupe(bool taupe) { this->m_taupe = taupe; }
+	static int getLargeur() { return largeur; }
+	static int getHauteur() { return hauteur; }
 	~Trou();
 };
 
