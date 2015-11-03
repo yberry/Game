b487e9483a0891c2e39d098d257e28a8b76c36b9
yberry <y_berry@hotmail.fr>
2015-11-03 16:26:03 +0100

    Commentaires

--- Trou.h
+++ Trou.h
@@ -5,19 +5,76 @@
 class Trou
 {
 private:
+	/*
+	Attribut m_taupe
+	Définit si la taupe est sortie de sa tanière ou non
+	*/
 	bool m_taupe;
+
+	/*
+	Attribut m_x
+	Numéro de la ligne sur laquelle se situe le coin haut gauche du trou
+	*/
 	int m_x;
+
+	/*
+	Attribut m_y
+	Numéro de la colonne sur laquelle se situe le coin haut gauche du trou
+	*/
 	int m_y;
+
+	/*
+	Attribut de classe largeur
+	Définit la largeur d'un trou
+	*/
 	static int largeur;
+
+	/*
+	Attribut de classe largeur
+	Définit la hauteur d'un trou
+	*/
 	static int hauteur;
 
 public:
+	/*
+	Constructeur Trou
+	x et y sont les coordonnées du coin haut gauche du trou
+	*/
 	Trou(int x, int y);
+
+	/*
+	Méthode draw
+	Permet d'afficher le trou à l'écran en utilisant un certain caractère
+	*/
 	void draw(CHAR_INFO* buffer, COORD bufferSize, char lettre) const;
+
+	/*
+	Méthode getTaupe
+	Permet de savoir si la taupe est sortie de sa tanière ou non
+	*/
 	bool getTaupe() const { return this->m_taupe; }
+
+	/*
+	Méthode setTaupe
+	Permet de configurer la sortie de la taupe
+	*/
 	void setTaupe(bool taupe) { this->m_taupe = taupe; }
+
+	/*
+	Méthode de classe getLargeur
+	Permet de connaître la largeur d'un trou
+	*/
 	static int getLargeur() { return largeur; }
+
+	/*
+	Méthode de classe getHauteur
+	Permet de connaître la hauteur d'un trou
+	*/
 	static int getHauteur() { return hauteur; }
+
+	/*
+	Destructeur
+	*/
 	~Trou();
 };
 
