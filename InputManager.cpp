3cf26fd6ad0c38bb03005e59aa7d1eafbd4a0279
Atutahi <fr.rivoire@gmail.com>
2015-11-03 16:13:34 +0100

    Update Docu InputManager.cpp

--- InputManager.cpp
+++ InputManager.cpp
@@ -10,10 +10,10 @@ Auteurs : François Rivoire et Yanis Berry
 
 
 InputManager::InputManager(HANDLE hInput, int nbInputs) :_hInput(hInput), _nbDetectedInputs(0)
-{
+{	
+	//Allocation du tableau de récupération des inputs :
 	_input_records.resize(nbInputs);
 }
-
 
 InputManager::~InputManager()
 {
@@ -23,6 +23,8 @@ InputManager::~InputManager()
 void InputManager::update()
 {
 	_nbDetectedInputs = 0;
+	
+	//Récupération des inputs -> mise à jour de _input_records et _nbDetectedInputs
 	ReadConsoleInput(_hInput, &_input_records[0], _input_records.size(), &_nbDetectedInputs);
 }
 
@@ -31,6 +33,7 @@ COORD InputManager::getMousePos()
 	COORD errMousePos;
 	errMousePos.X = errMousePos.Y = -1;
 
+	//Si le premier input détecté n'est pas un input de souris, on a un erreur.
 	if (_input_records[0].EventType != MOUSE_EVENT)
 		return errMousePos;
 
@@ -39,11 +42,14 @@ COORD InputManager::getMousePos()
 
 int InputManager::isMouseLeftButtonPressed()
 {
+	//Si le premier input détecté n'est pas un input de souris, on a un erreur.
 	if (_input_records[0].EventType != MOUSE_EVENT)
 		return -1;
 
+	//Si le boutton gauche est enfoncé, on renvoie 1
 	if (_input_records[0].Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
 		return 1;
 
+	//Sinon 0
 	return 0;
 }
