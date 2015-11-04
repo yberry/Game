6b6d339f198b1dadd2cabe71ef92f9386c9e503b
yberry <y_berry@hotmail.fr>
2015-11-04 15:58:48 +0100

    Commentaires

--- GameManager.h
+++ GameManager.h
@@ -1,19 +1,50 @@
 #pragma once
-#include <Windows.h>
+
 #include "Map.h"
 #include "OutputManager.h"
+#include "Joueur.h"
 
 class GameManager
 {
-private :
+private:
+	/*
+	Attribut oManager
+	Gère l'affichage
+	*/
 	OutputManager oManager;
+
+	/*
+	Attribut map
+	Permet de gérer la liste des trous et les taupes
+	*/
 	Map map;
 
+	/*
+	Attribut joueur
+	Permet de calculer le score et de gérer les clics de souris
+	*/
+	Joueur joueur;
+
 public:
+	/*
+	Constructeur
+	*/
 	GameManager();
-	~GameManager();
-
+	
+	/*
+	Méthode init
+	Permet de créer les trous
+	*/
 	void init();
-	void gameLoop();
-};
 
+	/*
+	Méthode gameLoop
+	Gère le jeu
+	*/
+	void gameLoop();
+
+	/*
+	Destructeur
+	*/
+	~GameManager();
+};
