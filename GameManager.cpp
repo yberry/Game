672b10de90cbcc1eb59dfd79cdd61c66aa9a421a
yberry <y_berry@hotmail.fr>
2015-11-04 15:58:32 +0100

    x et y

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,10 +1,11 @@
 #include "stdafx.h"
 #include "GameManager.h"
 
-
-GameManager::GameManager():oManager()
+GameManager::GameManager() :
+oManager(),
+joueur()
 {
-	
+
 }
 
 
@@ -16,21 +17,25 @@ void GameManager::init()
 {
 	oManager.init();
 
-	map.ajouterTrou(20, 50);
-	map.ajouterTrou(20, 100);
+	map.ajouterTrou(50, 20);
+	map.ajouterTrou(100, 20);
 	map.ajouterTrou(40, 40);
-	map.ajouterTrou(40, 75);
-	map.ajouterTrou(40, 110);
-	map.ajouterTrou(60, 50);
-	map.ajouterTrou(60, 100);
+	map.ajouterTrou(75, 40);
+	map.ajouterTrou(110, 40);
+	map.ajouterTrou(50, 60);
+	map.ajouterTrou(100, 60);
 }
 
 void GameManager::gameLoop()
 {
 	while (!GetAsyncKeyState(VK_ESCAPE)) {
 		oManager.read();
-		
+
 		map.updateTaupe();
+		COORD clic = joueur.jouer();
+		if (clic.X != -1 && clic.Y != -1) {
+			bool verif = map.hit(clic);
+		}
 		map.draw((CHAR_INFO *)oManager.buffer, oManager.dwBufferSize);
 
 		oManager.draw();
