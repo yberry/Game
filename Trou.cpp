8caa2593230114498242d2aac5d2f39e37c13867
yberry <y_berry@hotmail.fr>
2015-11-04 15:55:31 +0100

    x et y

--- Trou.cpp
+++ Trou.cpp
@@ -22,25 +22,42 @@ m_taupe(false)
 Méthode pour dessiner un trou avec le buffer de la console
 */
 void Trou::draw(CHAR_INFO* buffer, COORD bufferSize, char lettre) const {
-	//Pour chaque ligne
-	for (int i(0); i < hauteur; i++) {
-		//Et pour chaque colonne
-		for (int j(0); j < largeur; j++) {
+	//Pour chaque colonne
+	for (int i(0); i < largeur; i++) {
+		//Et pour chaque ligne
+		for (int j(0); j < hauteur; j++) {
 			//Sauf les coins
-			if ((i != 0 && i != hauteur - 1) || (j != 0 && j != largeur - 1)) {
-				//On ajoute la lettre dans le buffer
-				(buffer + ((m_x + i)*bufferSize.X + m_y + j))->Char.AsciiChar = lettre;
+			if ((i != 0 && i != largeur - 1) || (j != 0 && j != hauteur - 1)) {
+				//On ajoute le caractère dans le buffer
+				(buffer + ((m_y + j)*bufferSize.X + m_x + i))->Char.AsciiChar = lettre;
 			}
 		}
 	}
 
-	//Pour chaque ligne à l'intérieur du trou
-	for (int i(1); i < hauteur - 1; i++) {
-		//Et pour chaque colonne à l'intérieur du trou
-		for (int j(1); j < largeur - 1; j++) {
+	//Pour chaque colonne à l'intérieur du trou
+	for (int i(1); i < largeur - 1; i++) {
+		//Et pour chaque ligne à l'intérieur du trou
+		for (int j(1); j < hauteur - 1; j++) {
 			//On remplit avec un autre caractère ou on l'enlève selon la présence de la taupe
-			(buffer + ((m_x + i)*bufferSize.X + m_y + j))->Char.AsciiChar = (this->m_taupe ? 'X' : ' ');
+			(buffer + ((m_y + j)*bufferSize.X + m_x + i))->Char.AsciiChar = (this->m_taupe ? 'X' : ' ');
 		}
+	}
+}
+
+/*
+Méthode qui permet de savoir si on a cliqué sur une taupe
+*/
+bool Trou::hit(COORD clic) {
+	//S'il n'y a pas de taupe
+	if (!this->m_taupe)
+		return false;
+
+	//S'il y a une taupe et que l'on clique dans le trou
+	if (m_x < clic.X && clic.X < (m_x + largeur - 1) && m_y < clic.Y && clic.Y < (m_y + hauteur - 1)) {
+		this->m_taupe = false;
+		return true;
+	} else {
+		return false;
 	}
 }
 
