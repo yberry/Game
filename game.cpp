dafdaaefb18ef9ff260fb86394bb6f6005deaa23
Atutahi <fr.rivoire@gmail.com>
2015-11-04 14:08:40 +0100

    Update GameManager

--- game.cpp
+++ game.cpp
@@ -3,54 +3,27 @@
 
 #include "stdafx.h"
 #define WIN32_LEAN_AND_MEAN
-#define SCREEN_WIDTH 200
-#define SCREEN_HEIGHT 80
+
 #include <iostream>
-#include "Map.h"
+#include "GameManager.h"
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	HANDLE hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
-
-	COORD dwBufferSize = { SCREEN_WIDTH, SCREEN_HEIGHT };
-
-	SetConsoleScreenBufferSize(hOutput, dwBufferSize);
-
-	//Maximiser la taille de la fenêtre
-	HWND hwnd = GetForegroundWindow();
-	ShowWindow(hwnd, SW_MAXIMIZE);
-
-	COORD dwBufferCoord = { 0, 0 };
-	SMALL_RECT rcRegion = { 0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1 };
-
-	CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
-
-	Map map;
-
-	map.ajouterTrou(5, 50);
-	map.ajouterTrou(5, 60);
-	map.ajouterTrou(15, 45);
-	map.ajouterTrou(15, 55);
-	map.ajouterTrou(15, 65);
-	map.ajouterTrou(25, 50);
-	map.ajouterTrou(25, 60);
-
-	while (!GetAsyncKeyState(VK_ESCAPE)) {
-
-		ReadConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize,
-			dwBufferCoord, &rcRegion);		
-
-		map.updateTaupe();
-		map.draw((CHAR_INFO *)buffer, dwBufferSize);
-
-		WriteConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize,
-			dwBufferCoord, &rcRegion);
-
-	}
+	GameManager gManager = GameManager();
+	
+	gManager.init();
+	gManager.gameLoop();
+	
 	return 0;
 }
+
+
+
+
+
+
 
 
 
