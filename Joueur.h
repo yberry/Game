024885f01dd323b522bcd9773ca9c708588f0188
yberry <y_berry@hotmail.fr>
2015-11-05 15:23:23 +0100

    Update Joueur.h

--- Joueur.h
+++ Joueur.h
@@ -22,8 +22,10 @@ private:
 	Attribut freq_marteau
 	Permet de régler le temps d'inactivité du marteau
 	*/
-	float freq_marteau = 1;
+	float freq_marteau = 0.5f;
 
+	bool frappe;
+	
 public:
 	/*
 	Constructeur
@@ -35,6 +37,12 @@ public:
 	Permet de récupérer les coordonnées du clic
 	*/
 	COORD jouer();
+
+	/*
+	Méthode draw
+	Permet de dessiner le marteau du joueur
+	*/
+	void draw(CHAR_INFO* buffer, COORD bufferSize);
 
 	/*
 	Destructeur
