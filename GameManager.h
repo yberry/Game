7fda1cb9493a11430fd38c4515204b7919faf13f
yberry <y_berry@hotmail.fr>
2015-11-05 14:27:30 +0100

    Finitions

--- GameManager.h
+++ GameManager.h
@@ -21,9 +21,33 @@ private:
 
 	/*
 	Attribut joueur
-	Permet de calculer le score et de gérer les clics de souris
+	Permet de gérer les clics de souris
 	*/
 	Joueur joueur;
+
+	/*
+	Attribut temps
+	Temps restant avant la fin de la partie
+	*/
+	int temps = 10;
+
+	/*
+	Attribut score
+	Temps total passé par le joueur à taper les taupes
+	*/
+	int score = 0;
+
+	/*
+	Attribut _time
+	Timer servant à faire s'écouler les secondes
+	*/
+	Timer _time;
+
+	/*
+	Attribut freq_score
+	Règle la fréquence du timer
+	*/
+	float freq_score = 1;
 
 public:
 	/*
@@ -42,6 +66,18 @@ public:
 	Gère le jeu
 	*/
 	void gameLoop();
+
+	/*
+	Méthode showScore
+	Permet d'afficher le temps restant du jeu à partir d'un point de coordonnées {x, y} donné
+	*/
+	void showTime(CHAR_INFO *buffer, COORD dwBufferSize, int x, int y) const;
+
+	/*
+	Méthode endGame
+	Permet d'effacer la map et d'afficher le score en fin de partie
+	*/
+	void endGame(CHAR_INFO *buffer, COORD dwBufferSize) const;
 
 	/*
 	Destructeur
